a7d25e83285be54bb8187cfae39b068991260c72
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 17:02:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,13 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
-  collection = []
-  collection.each do |languages|
-    languages.upcase
-end
-  collection
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
 end
 
+my_collect(list) {|i| i.split(" ").first}
